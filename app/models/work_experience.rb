c0d85f6c9e20a52baa740bbb3c4487e940cb1c52
Linh <tomroket@gmail.com>
2019-05-22 16:50:34 +0800

    Validates endtime

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -2,11 +2,11 @@ class WorkExperience < ApplicationRecord
   belongs_to :user
   validates :description,  presence: true, length: { maximum: 100 }
   validates :start_time, presence: true
-  validate :end_date_after_start_date?
+  validate :end_date_after_start_date, if -> { end_time.presence && start_time.presence }
 
-	def end_date_after_start_date?
-	  if end_date < start_date
-	    errors.add :end_date, "Must be after start date"
-	  end
+	def end_date_after_start_date
+    if end_time < start_time
+      errors.add :end_time, "Must be after start date"
+    end
 	end
 end
