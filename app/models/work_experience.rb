928aacc86362a3ca5bcf6e388b1b162d027e34ca
Linh <tomroket@gmail.com>
2019-05-22 15:29:10 +0800

    52 remove end_time

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -2,7 +2,6 @@ class WorkExperience < ApplicationRecord
   belongs_to :user
   validates :description,  presence: true, length: { maximum: 100 }
   validates :start_time, presence: true
-  validates :end_time, presence: true
   validate :end_date_after_start_date?
 
 	def end_date_after_start_date?
