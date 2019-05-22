879ad75e6a3c8f560983bdd256d09a008e741a5b
Linh <tomroket@gmail.com>
2019-05-22 18:21:40 +0800

    changing content validate

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
-  validates :content, presence: true, length: { minimum: 300 }
+  validates :content, presence: true, length: { maximum: 300 }
 end
