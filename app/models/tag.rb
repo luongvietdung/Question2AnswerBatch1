d465a26b73e6fa543f94a4fd1c718b37acabab97
Linh <tomroket@gmail.com>
2019-05-23 17:28:04 +0800

    Add has_and_belongs_to_many between tags and questions

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ApplicationRecord
   validates :content, presence: true
+  has_and_belongs_to_many :questions
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190523083447) do
+ActiveRecord::Schema.define(version: 20190523092356) do
 
   create_table "answers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text "content"
@@ -41,6 +41,11 @@ ActiveRecord::Schema.define(version: 20190523083447) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_questions_on_user_id"
+  end
+
+  create_table "questions_tags", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "question_id", null: false
+    t.bigint "tag_id", null: false
   end
 
   create_table "tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
