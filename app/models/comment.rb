8968611ba86582bb8f20c30ae206c64d99b2afa3
Linh <tomroket@gmail.com>
2019-05-23 09:38:08 +0800

    add user reference with comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
-  belongs_to :users
+  belongs_to :user
   validates :content, presence: true, length: { maximum: 300 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,11 @@ ActiveRecord::Schema.define(version: 20190522093315) do
     t.text "content"
     t.string "commentable_type"
     t.bigint "commentable_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -41,5 +43,6 @@ ActiveRecord::Schema.define(version: 20190522093315) do
     t.index ["user_id"], name: "index_work_experiences_on_user_id"
   end
 
+  add_foreign_key "comments", "users"
   add_foreign_key "work_experiences", "users"
 end
