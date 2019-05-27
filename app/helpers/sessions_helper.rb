1093245e77a5a7354a5ecfc1a142cbe008de4d23
Linh <tomroket@gmail.com>
2019-05-27 11:03:52 +0800

    rebase

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,12 +1,12 @@
 module SessionsHelper
   def log_in(user)
-    session[:user_id] = user.id
+   session[:user_id] = user.id
   end
 
   def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
+   user.remember
+   cookies.permanent.signed[:user_id] = user.id
+   cookies.permanent[:remember_token] = user.remember_token
   end
 
   def current_user
@@ -26,7 +26,13 @@ module SessionsHelper
   end
 
   def logged_in?
-    !current_user.nil?
+   !current_user.nil?
+  end
+
+  def forget(user)
+   user.forget
+   cookies.delete(:user_id)
+   cookies.delete(:remember_token)
   end
 
   def forget(user)
@@ -46,3 +52,4 @@ module SessionsHelper
     session[:forwarding_url] = request.original_url if request.get?
   end
 end
+
