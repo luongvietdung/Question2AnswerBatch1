7f0f9706d414cdb32317d68069d844619fb926a6
Linh <tomroket@gmail.com>
2019-05-28 18:07:55 +0800

    fix comment

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,24 +1,24 @@
 module SessionsHelper
   def log_in(user)
-   session[:user_id] = user.id
+    session[:user_id] = user.id
   end
 
   def remember(user)
-   user.remember
-   cookies.permanent.signed[:user_id] = user.id
-   cookies.permanent[:remember_token] = user.remember_token
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   def current_user
-   if (user_id = session[:user_id])
+    if (user_id = session[:user_id])
      @current_user ||= User.find_by(id: user_id)
-   elsif (user_id = cookies.signed[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
      user = User.find_by(id: user_id)
      if user && user.authenticated?(cookies[:remember_token])
        log_in user
        @current_user = user
      end
-   end
+    end
   end
 
   def current_user?(user)
@@ -26,13 +26,7 @@ module SessionsHelper
   end
 
   def logged_in?
-   !current_user.nil?
-  end
-
-  def forget(user)
-   user.forget
-   cookies.delete(:user_id)
-   cookies.delete(:remember_token)
+    !current_user.nil?
   end
 
   def forget(user)
@@ -42,13 +36,13 @@ module SessionsHelper
   end
 
   def log_out
-   forget(@current_user)
-   @current_user.forget
-   session.delete(:user_id)
-   @current_user = nil
+    forget(@current_user)
+    @current_user.forget
+    session.delete(:user_id)
+    @current_user = nil
   end
 
   def store_location
-  session[:forwarding_url] = request.original_url if request.get?
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
