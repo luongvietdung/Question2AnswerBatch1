b7f74abcd69e2fc07cebc8b39c41a519f355160d
Linh <tomroket@gmail.com>
2019-05-29 11:01:25 +0800

    fix comment 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   def logged_in_user
-    unless logged_in?
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
+    return (redirect_to login_url; flash[:danger] = "Please log in.") if !logged_in?
   end
 end
