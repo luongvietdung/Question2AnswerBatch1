7f768548a74310fbc6210b38b83ad0818c378128
ninhpham16 <ninhphamk41sp2@gmail.com>
2019-05-29 12:03:03 +0700

    82-home-page
    82-home-page
    
    82-home-page
    
    82-home-page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,18 +1,24 @@
 class QuestionsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create]
 
+  def index
+  end
+
   def new
     @question = Question.new
   end
 
   def create
     @question = current_user.questions.build(question_params)
-    if @question.save
-      flash[:success] = "Question created!"
-      redirect_to root_url
-    else
-      render :new
-    end
+      if @question.save
+        flash[:success] = "Question created!"
+        redirect_to root_url
+      else
+        render :new
+      end
+  end
+
+  def show
   end
 
   private
